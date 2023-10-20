3daf89b44ca0e57d17d655b6e404f3a3aa9c0707
Jonathan Lefebvre <jonathan@hop3team.com>
2023-10-20 21:37:46 +0200

    ✨ add actions and reducers for Marvel

---
+++ src/actions/marvel.action.js
@@ -0,0 +1,21 @@
+import {
+  MARVEL_SET_CHARACTERS_ACTION,
+  MARVEL_LOADING_ACTION,
+  MARVEL_ERROR_ACTION,
+} from '../reducers';
+
+export const setMarvelLoading = () => ({
+  type: MARVEL_LOADING_ACTION,
+  payload: null,
+});
+
+export const setMarvelError = (errorMessage) => ({
+  type: MARVEL_ERROR_ACTION,
+  payload: { error: errorMessage },
+});
+
+
+export const setMarvelCharacters = (characters, pagination) => ({
+  type: MARVEL_SET_CHARACTERS_ACTION,
+  payload: { characters, pagination },
+});
