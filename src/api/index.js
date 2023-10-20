2007b5ef6a0e26a01ae03f1a07dc49ca9013a878
Jonathan Lefebvre <jonathan@hop3team.com>
2023-10-20 20:09:17 +0200

    ✨ add authentication to use Marvel's API

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,26 @@
 import axios from 'axios';
+import md5 from 'md5';
 
 const instance = axios.create({
   baseURL: 'https://gateway.marvel.com/v1/public/',
-  timeout: 1000,
+  timeout: 3000,
 });
 
-const baseQueryParams = {
-  apikey: process.env.REACT_APP_MARVEL_API_KEY,
+// https://developer.marvel.com/documentation/authorization
+const baseQueryParams = () => {
+  const ts = Date.now();
+
+  return {
+    ts,
+    apikey: process.env.REACT_APP_MARVEL_API_KEY,
+    hash: md5(`${ts}${process.env.REACT_APP_MARVEL_PRIVATE_API_KEY}${process.env.REACT_APP_MARVEL_API_KEY}`),
+  };
 };
 
 const get = (url, query) => instance.get(url, {
   params: {
     ...query,
-    ...baseQueryParams,
+    ...baseQueryParams(),
   },
 });
 
