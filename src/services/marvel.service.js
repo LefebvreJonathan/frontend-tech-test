0de9515ee950d6cfc2a2d96b1d3b88e080d2cc99
Jonathan Lefebvre <38315793+LefebvreJonathan@users.noreply.github.com>
2023-10-22 14:29:24 +0200

    ✨ Add pagination component (#4)
    * 🐛 Fix currentPage data
    
    * ✨ Add Pagination Component
    
    * ✨ Use pagination component in App

--- src/services/marvel.service.js
+++ src/services/marvel.service.js
@@ -5,7 +5,7 @@ import MarvelCharacter from '../models/marvel/marvelCharacter';
 const DEFAULT_PAGE = 0;
 const DEFAULT_ITEMS_PER_PAGE = 4;
 
-const initPagination = (total, currentPage, itemsPerPage) => {
+const initPagination = (total, offset, itemsPerPage) => {
   const remainder = total % itemsPerPage;
 
   const pageCount = (total - remainder) / itemsPerPage + (total % itemsPerPage > 0 ? 1 : 0);
@@ -13,7 +13,7 @@ const initPagination = (total, currentPage, itemsPerPage) => {
   return {
     totalItems: total,
     itemsPerPage,
-    currentPage,
+    currentPage: offset / itemsPerPage,
     pagesCount: total !== 0 ? pageCount : 0,
   };
 };
@@ -26,7 +26,7 @@ const transformCharacters = (rawCharacters) => rawCharacters.map(({
   name, description: removeReplacementCharacter(description), thumbnail: `${thumbnail.path}.${thumbnail.extension}`, comicsCount: comics.available, seriesCount: series.available, storiesCount: stories.available,
 }));
 
-export const searchCharacters = (search, page = DEFAULT_PAGE, itemsPerPage = DEFAULT_ITEMS_PER_PAGE) => get(GET_CHARACTERS_ROUTE, { nameStartsWith: search, limit: itemsPerPage, offset: page })
+export const searchCharacters = (search, page = DEFAULT_PAGE, itemsPerPage = DEFAULT_ITEMS_PER_PAGE) => get(GET_CHARACTERS_ROUTE, { nameStartsWith: search, limit: itemsPerPage, offset: itemsPerPage * page })
   .then((response) => {
     const { data: { data } } = response;
 
