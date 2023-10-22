c4956d2ef45f8896bc9653fc82d3aece1638c454
Jonathan Lefebvre <jonathan@hop3team.com>
2023-10-22 20:12:11 +0200

    ✨ Add loading indicator

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,21 +1,28 @@
 /* eslint-disable react/no-unescaped-entities */
 import React, { useReducer, useState } from 'react';
-import './App.scss';
+
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
-import Header from '../components/Header';
+
+import {
+  setMarvelLoading, setMarvelError, setMarvelCharacters,
+} from '../actions';
+
 import CharacterBanner from '../components/CharacterBanner';
+import Header from '../components/Header';
+import Pagination from '../components/Pagination';
+
 import { searchCharacters } from '../services';
 import {
   marvelReducer, marvelDefaultState,
 } from '../reducers';
-import {
-  setMarvelLoading, setMarvelError, setMarvelCharacters,
-} from '../actions';
-import Pagination from '../components/Pagination';
+
+import LoadingPage from './Loading';
+
+import './App.scss';
 
 const KEYS_NEED_PRESSED_TO_CONTINUE = ['Enter'];
 const MINIMUM_LENGTH_TO_SUBMIT = 0;
@@ -28,6 +35,7 @@ function App() {
 
 
   const loadCharacters = (page) => {
+    dispatch(setMarvelCharacters([], { }));
     dispatch(setMarvelLoading());
     searchCharacters(search, page).then(({ characters, pagination }) => dispatch(setMarvelCharacters(characters, pagination, search)))
       .catch((error) => dispatch(setMarvelError(error.message)));
@@ -40,23 +48,21 @@ function App() {
   };
 
   return (
-	<>
-		<Router>
-			<Header onChange={(value) => setSearch(value)} onKeyPress={handleChange} />
-			<Switch>
-				<Route
-					exact
-					path="/"
-				>
-					<section className="lumx-spacing-padding-horizontal-huge characters-list">
-						{marvelState.characters.map((character) => <CharacterBanner key={character.name} character={character} />)}
-					</section>
-					<Pagination currentPage={marvelState.pagination.currentPage + OFFSET_PAGE} totalPages={marvelState.pagination.pagesCount} onChangePage={(page) => loadCharacters(page - OFFSET_PAGE)} />
-				</Route>
-			</Switch>
-		</Router>
-
-	</>
+	<Router>
+		<Header onChange={(value) => setSearch(value)} onKeyPress={handleChange} />
+		<Switch>
+			<Route
+				exact
+				path="/"
+			>
+				{marvelState.loading && <LoadingPage />}
+				<section className="lumx-spacing-padding-horizontal-huge characters-list">
+					{marvelState.characters.map((character) => <CharacterBanner key={character.name} character={character} />)}
+				</section>
+				<Pagination currentPage={marvelState.pagination.currentPage + OFFSET_PAGE} totalPages={marvelState.pagination.pagesCount} onChangePage={(page) => loadCharacters(page - OFFSET_PAGE)} />
+			</Route>
+		</Switch>
+	</Router>
   );
 }
 
