7249c872d2dd3976172cf1ad33cd4e3de803ff22
Jonathan Lefebvre <38315793+LefebvreJonathan@users.noreply.github.com>
2023-10-22 15:07:06 +0200

    ✨ add logo in header (#5)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { FlexBox, Alignment } from '@lumx/react';
 import PropTypes from 'prop-types';
 import Search from '../Search';
 
 const Header = ({ onChange, onKeyPress }) => (
 	<header className="lumx-spacing-padding-big header">
-		<FlexBox vAlign={Alignment.right}>
-			<Search onChange={onChange} onKeyPress={onKeyPress} />
-		</FlexBox>
+		<img src={`${process.env.PUBLIC_URL}/marvel_logo.png`} alt="marvel" className="logo" loading="lazy" />
+		<Search onChange={onChange} onKeyPress={onKeyPress} />
 	</header>
 );
 
